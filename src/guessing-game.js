ecf3acf08cd21f9e6964e4916eae96d7900018e1
Alina-13 <alina.za13@gmail.com>
2024-10-26 02:59:00 -0400

    feat; test

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.currentGuess = Math.floor((this.min + this.max) / 2);
+        return this.currentGuess;
     }
 
     lower() {
-
+        this.max = this.currentGuess - 1;
     }
 
     greater() {
-
+        this.min = this.currentGuess + 1;
     }
 }
 
